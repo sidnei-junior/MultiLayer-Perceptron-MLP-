ab75acd7a019f5b61263a374f63741dfc3281775
Sidnei Jr <sidnei.ifrn@gmail.com>
2019-05-19 17:14:28 -0300

    Estrutura base do construtor e funções

--- camada.py
+++ camada.py
@@ -1,1 +1,11 @@
-import "neuronio.py"
+from neuronio import neuronio
+
+class camada:
+
+    def __init__(self, entradas, pesos, n_neuronios, saidas, limiares):
+
+    def forward(self):
+        # Chamar o forward da classe neurônio
+
+    def backward(self):
+        # Chamar o backward da classe neurônio
